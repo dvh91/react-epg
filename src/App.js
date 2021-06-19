85940ba30f7f6c88615e57b7a0ffb32fb70f7f0c
Dvir Hazout <dvir.hazout@kaltura.com>
2021-06-19 19:26:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,419 @@
+import "./styles.css";
+import { channels, DAYS_BACK_MILLIS } from "./data";
+import { addDays, format, setMinutes, subDays } from "date-fns";
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState
+} from "react";
+import { throttle } from "lodash";
+
+export const minute = 60 * 1000;
+export const hour = 60 * minute;
+export const day = 24 * hour;
+const channelHeight = 100;
+const nowMillis = setMinutes(new Date(), 0).getTime();
+
+export const LIST_WIDTH = 600;
+export const LIST_HEIGHT = channelHeight * 4;
+export const HOURS_IN_SCREEN = 3;
+export const HOUR_WIDTH = LIST_WIDTH / HOURS_IN_SCREEN;
+export const EPG_START_DATA_TIME = nowMillis - 10 * day;
+export const EPG_END_DATA_TIME = nowMillis + 10 * day;
+
+export const epgEdges = {
+  start: EPG_START_DATA_TIME,
+  end: EPG_END_DATA_TIME
+};
+
+export const getWidthByTime = (time) => {
+  return HOUR_WIDTH * (time / hour);
+};
+
+export const getProgramWidth = (start, end) => {
+  return getWidthByTime(end - start);
+};
+
+export const isInTimerange = (time, start, end) => {
+  return time >= start && time < end;
+};
+
+export const isLive = (start, end) => {
+  const now = Date.now();
+  return isInTimerange(now, start, end);
+};
+
+const Program = forwardRef(
+  ({ data, isFocused, channelIndex, onUnmount }, ref) => {
+    const rootRef = useRef(null);
+    const [isFuture, setIsFuture] = useState(() => {
+      const isLive = Date.now() >= data.start && Date.now() < data.end;
+      return !isLive && Date.now() < data.end;
+    });
+
+    useImperativeHandle(ref, () => ({
+      id: data.id,
+      focus: () => {
+        rootRef.current.focus();
+      }
+    }));
+
+    useEffect(() => {
+      return () => {
+        onUnmount(data.id);
+      };
+    }, []);
+
+    const getProgramStyle = ({ program, channelIndex }) => {
+      const left = getWidthByTime(
+        program.start - (Date.now() - DAYS_BACK_MILLIS)
+      );
+
+      const width = getProgramWidth(program.start, program.end);
+      const height = channelHeight;
+
+      return {
+        position: "absolute",
+        top: channelIndex * channelHeight + 36,
+        left,
+        width,
+        height,
+        backgroundColor: isFocused ? "#ccc" : undefined,
+        opacity: isFuture ? 0.5 : 1
+      };
+    };
+
+    return (
+      <div
+        ref={rootRef}
+        className="program"
+        tabIndex="0"
+        style={getProgramStyle({ program: data, channelIndex })}
+      >
+        <div className="program-title">{data.title}</div>
+        <div className="program-times">
+          {format(data.start, "HH:mm")} - {format(data.end, "HH:mm")}
+        </div>
+      </div>
+    );
+  }
+);
+
+const Epg = forwardRef(
+  ({ data, initialFocusedChannel, initialFocusedProgram }, ref) => {
+    const [offsetX, setOffsetX] = useState(0);
+    const [offsetY, setOffsetY] = useState(0);
+    const offsetTime = useRef(Date.now());
+    const containerRef = useRef();
+    const programRefs = useRef({});
+    const [focusedChannelIndex, setFocusedChannelIndex] = useState(() =>
+      data.findIndex((c) => initialFocusedChannel?.number === c.number)
+    );
+    const [focusedProgram, setFocusedProgram] = useState(initialFocusedProgram);
+
+    const unmountedFocusedProgramId = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+      scrollToTime,
+      focusProgram: (program, channel) => {
+        setFocusedChannelIndex(channels.indexOf(channel));
+        setFocusedProgram(program);
+      }
+    }));
+
+    const scrollToTime = useCallback((time, behavior = "smooth") => {
+      offsetTime.current = time;
+      let left = getWidthByTime(time - (Date.now() - DAYS_BACK_MILLIS));
+      left -= LIST_WIDTH / 2;
+
+      containerRef.current.scrollTo({
+        left,
+        behavior
+      });
+    }, []);
+
+    const scrollToProgram = useCallback(
+      (program, isSmoothScroll) => {
+        const time = program.start + (program.end - program.start) / 2;
+
+        scrollToTime(time, isSmoothScroll ? "smooth" : "auto");
+      },
+      [scrollToTime]
+    );
+
+    useEffect(() => {
+      if (!initialFocusedProgram) return;
+      scrollToProgram(initialFocusedProgram, false);
+    }, [initialFocusedProgram, scrollToProgram]);
+
+    useEffect(() => {
+      if (!focusedProgram) return;
+      if (!programRefs.current[focusedProgram.id]) {
+        unmountedFocusedProgramId.current = focusedProgram.id;
+        return;
+      }
+      programRefs.current[focusedProgram.id].focus();
+    }, [focusedProgram]);
+
+    const isProgramVisible = ({ program, channelIndex }) => {
+      if (offsetY > channelIndex * channelHeight + 4 * channelHeight) {
+        return false;
+      }
+      if (offsetY < channelIndex * channelHeight - 4 * channelHeight) {
+        return false;
+      }
+      const programLeft = getWidthByTime(
+        program.start - (Date.now() - DAYS_BACK_MILLIS)
+      );
+      const programWidth = getWidthByTime(program.end - program.start);
+      const programRight = programLeft + programWidth;
+
+      if (
+        programLeft > offsetX - programWidth &&
+        programRight - programWidth < offsetX + LIST_WIDTH
+      )
+        return true;
+
+      return false;
+    };
+
+    const isTimeVisible = (time) => {
+      const left = getWidthByTime(time - (Date.now() - DAYS_BACK_MILLIS));
+      const width = hour;
+      const right = left + width;
+
+      if (left > offsetX - width && right - width < offsetX + LIST_WIDTH)
+        return true;
+
+      return false;
+    };
+
+    const handleProgramRef = useCallback((ref) => {
+      if (!ref) return;
+      programRefs.current[ref.id] = ref;
+
+      if (unmountedFocusedProgramId.current === ref.id) {
+        ref.focus();
+        unmountedFocusedProgramId.current = null;
+      }
+    }, []);
+
+    const handleProgramUnmount = useCallback((programId) => {
+      programRefs.current[programId] = null;
+    }, []);
+
+    useEffect(() => {
+      containerRef.current.addEventListener(
+        "scroll",
+        throttle((e) => {
+          setOffsetX(e.target.scrollLeft);
+          setOffsetY(e.target.scrollTop);
+        }, 100)
+      );
+    }, []);
+
+    const handleKeyDown = useCallback(
+      (e) => {
+        if (e.which !== 38 && e.which !== 40) return;
+
+        e.preventDefault();
+
+        setFocusedChannelIndex((prev) => {
+          const next = e.which === 40 ? prev + 1 : prev - 1;
+          containerRef.current.scrollTo({
+            top: next * channelHeight,
+            behavior: "smooth"
+          });
+
+          const nextProgram = data[next].programs.find(
+            (program) =>
+              offsetTime.current >= program.start &&
+              offsetTime.current < program.end
+          );
+
+          setFocusedProgram(nextProgram);
+
+          return next;
+        });
+      },
+      [data]
+    );
+
+    useEffect(() => {
+      window.addEventListener("keyup", handleKeyDown);
+
+      return () => {
+        window.removeEventListener("keyup", handleKeyDown);
+      };
+    }, [handleKeyDown]);
+
+    const [times, setTimes] = useState(() => {
+      let time = epgEdges.start;
+      const result = [time];
+
+      while (time < epgEdges.end) {
+        time += hour;
+        result.push(time);
+      }
+
+      return result;
+    });
+
+    return (
+      <div className="epg" style={{ width: LIST_WIDTH }}>
+        <div className="details">
+          <div style={{ marginBottom: 8 }}>{focusedProgram?.title}</div>
+          <div style={{ fontSize: 14, opacity: 0.7 }}>
+            {focusedProgram?.description}
+          </div>
+        </div>
+        <div
+          ref={containerRef}
+          style={{
+            height: channelHeight * 4,
+            overflow: "auto",
+            position: "relative"
+          }}
+        >
+          <div
+            style={{
+              position: "relative",
+              width: getWidthByTime(epgEdges.end - epgEdges.start),
+              height: channels.length * channelHeight
+            }}
+          >
+            <div
+              style={{
+                backgroundColor: "#ccc",
+                position: "absolute",
+                top: 0,
+                left: getWidthByTime(Date.now() - epgEdges.start),
+                bottom: 0,
+                width: 4,
+                zIndex: 10
+              }}
+            />
+            <div className="times">
+              {times.filter(isTimeVisible).map((time) => (
+                <div
+                  style={{
+                    position: "absolute",
+                    left: getWidthByTime(time - epgEdges.start),
+                    zIndex: 10,
+                    display: "flex",
+                    alignItems: "center"
+                  }}
+                >
+                  {format(time, "HH:mm")}
+                </div>
+              ))}
+            </div>
+            <div className="channels">
+              {data.map((channel, channelIndex) => (
+                <div
+                  style={{
+                    height: channelHeight,
+                    padding: 4,
+                    display: "flex"
+                  }}
+                >
+                  <img src={channel.logo} width={60} height={60} />
+                </div>
+              ))}
+            </div>
+            {data.map((channel, channelIndex) =>
+              channel.programs
+                .filter((program) =>
+                  isProgramVisible({ program, channelIndex })
+                )
+                .map((program) => (
+                  <Program
+                    key={program.id}
+                    ref={handleProgramRef}
+                    onUnmount={handleProgramUnmount}
+                    data={program}
+                    channelIndex={channelIndex}
+                    isFocused={focusedProgram?.id === program.id}
+                  />
+                ))
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default function App() {
+  const epgRef = useRef();
+
+  const [times, setTimes] = useState([
+    subDays(new Date(), 10),
+    subDays(new Date(), 9),
+    subDays(new Date(), 8),
+    subDays(new Date(), 7),
+    subDays(new Date(), 6),
+    subDays(new Date(), 5),
+    subDays(new Date(), 4),
+    subDays(new Date(), 3),
+    subDays(new Date(), 2),
+    subDays(new Date(), 1),
+    ["Now", new Date()],
+    addDays(new Date(), 1),
+    addDays(new Date(), 2),
+    addDays(new Date(), 3),
+    addDays(new Date(), 4),
+    addDays(new Date(), 5),
+    addDays(new Date(), 6),
+    addDays(new Date(), 7),
+    addDays(new Date(), 8),
+    addDays(new Date(), 9),
+    addDays(new Date(), 10)
+  ]);
+
+  return (
+    <div className="App">
+      <div
+        ref={(ref) => (ref.scrollLeft = ref.offsetWidth / 2)}
+        style={{
+          width: 600,
+          whiteSpace: "nowrap",
+          overflow: "scroll",
+          padding: 8,
+          backgroundColor: "#f8f8f8"
+        }}
+      >
+        {times.map((item) => {
+          const date = Array.isArray(item) ? item[1] : item;
+          const label = Array.isArray(item) ? item[0] : format(date, "dd/MM");
+          return (
+            <button
+              className="day-button"
+              onClick={() => {
+                const time = date.getTime();
+                const channel = channels[0];
+                const program = channel.programs.find((p) =>
+                  isInTimerange(time, p.start, p.end)
+                );
+                epgRef.current?.scrollToTime(time);
+                epgRef.current?.focusProgram(program, channel);
+              }}
+            >
+              {label}
+            </button>
+          );
+        })}
+      </div>
+
+      <Epg
+        ref={epgRef}
+        data={channels}
+        initialFocusedChannel={channels[1]}
+        initialFocusedProgram={channels[1].programs[10]}
+      />
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,115 @@
+import { setMinutes } from "date-fns";
+
+const availableEvents = [
+  {
+    title: "Avengers",
+    image:
+      "https://fanart.tv/fanart/movies/99861/moviethumb/avengers-age-of-ultron-55ff4a081b4f4.jpg",
+    description:
+      "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
+  },
+  {
+    title: "How I Met Your Mother",
+    image:
+      "https://fanart.tv/fanart/tv/75760/tvthumb/how-i-met-your-mother-5c05179b4f6c0.jpg",
+    description:
+      "A father recounts to his children - through a series of flashbacks - the journey he and his four best friends took leading up to him meeting their mother."
+  },
+  {
+    title: "Silicon Valley",
+    image:
+      "https://fanart.tv/fanart/tv/277165/tvthumb/silicon-valley-559d97ec079f3.jpg",
+    description:
+      "Follows the struggle of Richard Hendricks, a Silicon Valley engineer trying to build his own company called Pied Piper."
+  },
+  {
+    title: "Late Night with Jimmy Fallon",
+    image:
+      "https://fanart.tv/fanart/tv/85355/tvthumb/late-night-with-jimmy-fallon-50b16e376c3f4.jpg",
+    description: "Comedian Jimmy Fallon hosts a late-night talk show."
+  },
+  {
+    title: "The Big Bang Theory",
+    image:
+      "https://fanart.tv/fanart/tv/80379/tvthumb/the-big-bang-theory-4f00d72c1c50b.jpg",
+    description:
+      "A woman who moves into an apartment across the hall from two brilliant but socially awkward physicists shows them how little they know about life outside of the laboratory."
+  },
+  {
+    title: "Leon",
+    image:
+      "https://fanart.tv/fanart/movies/101/moviethumb/leon-the-professional-535fdab0b01c8.jpg",
+    description:
+      "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. An unusual relationship forms as she becomes his protégée and learns the assassin's trade."
+  }
+];
+
+const availableChannelLogos = [
+  "http://s3-eu-west-1.amazonaws.com/rockettv.media.images/popcorn/images/channels/v3/logos/default/CNN_88.png",
+  "http://s3-eu-west-1.amazonaws.com/rockettv.media.images/popcorn/images/channels/v3/logos/default/MB1_88.png",
+  "http://s3-eu-west-1.amazonaws.com/rockettv.media.images/popcorn/images/channels/v3/logos/default/NGO_88.png",
+  "http://s3-eu-west-1.amazonaws.com/rockettv.media.images/popcorn/images/channels/v3/logos/default/FXH_60.png",
+  "http://s3-eu-west-1.amazonaws.com/rockettv.media.images/popcorn/images/channels/v3/logos/default/TRM_88.png"
+];
+
+const availableEventLength = [
+  1000 * 60 * 15, // 15 minutes
+  1000 * 60 * 30, // 30 minutes
+  1000 * 60 * 45, // 45 minutes
+  1000 * 60 * 60, // 60 minutes
+  1000 * 60 * 120 // 120 minutes
+];
+
+export const DAYS_BACK_MILLIS = 10 * 24 * 60 * 60 * 1000; // 10 days
+export const DAYS_FORWARD_MILLIS = 10 * 24 * 60 * 60 * 1000; // 10 days
+
+const getEventEnd = (eventStartMillis) => {
+  const length = availableEventLength[Math.floor(Math.random() * 4 + 0)];
+  return eventStartMillis + length;
+};
+
+const createEvents = (epgChannel, nowMillis) => {
+  const events = new Array();
+
+  const epgStart = nowMillis - DAYS_BACK_MILLIS;
+  const epgEnd = nowMillis + DAYS_FORWARD_MILLIS;
+
+  let currentTime = epgStart;
+
+  while (currentTime <= epgEnd) {
+    const eventEnd = getEventEnd(currentTime);
+    const event = availableEvents[Math.floor(Math.random() * 6 + 0)];
+    const epgEvent = {
+      id: currentTime + epgChannel.number,
+      start: currentTime,
+      end: eventEnd - 1,
+      title: event.title,
+      image: event.image,
+      description: event.description
+    };
+    events.push(epgEvent);
+    currentTime = eventEnd;
+  }
+
+  return events;
+};
+
+const generateTVGuide = () => {
+  const channels = new Array();
+
+  const nowMillis = setMinutes(new Date(), 0).getTime();
+
+  for (let i = 0; i < 100; i++) {
+    const epgChannel = {
+      number: i + 1,
+      logo: availableChannelLogos[i % 5]
+    };
+
+    epgChannel.programs = createEvents(epgChannel, nowMillis);
+    channels.push(epgChannel);
+  }
+
+  return channels;
+};
+
+export const channels = generateTVGuide();
