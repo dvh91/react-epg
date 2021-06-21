759a72db50c549e9adf1d0a1932b041de7d62cc2
Dvir Hazout <dvir.hazout@kaltura.com>
2021-06-21 18:13:51 +0300

    fixing days bar active item

--- src/DaysBar.js
+++ src/DaysBar.js
@@ -1,5 +1,13 @@
-import { addDays, format, getHours, setHours } from "date-fns";
-import { useCallback, useState } from "react";
+import {
+  addDays,
+  addHours,
+  endOfDay,
+  format,
+  getHours,
+  setHours,
+  startOfDay
+} from "date-fns";
+import { useCallback, useEffect, useState } from "react";
 
 const primeTimeHour = 20;
 
@@ -7,16 +15,22 @@ const DaysBar = ({ value, onSelect }) => {
   const [times] = useState(() => {
     const result = [];
     for (let index = -10; index < 10; index++) {
-      if (index === 0) result.push([new Date(), "Now"]);
-      else result.push([addDays(new Date(), index)]);
+      if (index === 0) result.push([new Date(), "Now", "day"]);
+      else result.push([addDays(new Date(), index), "", "day"]);
     }
 
     if (getHours(new Date()) + 3 < primeTimeHour + 2) {
-      result.push([setHours(new Date(), primeTimeHour), "This evening"]);
+      result.push([
+        setHours(new Date(), primeTimeHour),
+        "This evening",
+        "primetime"
+      ]);
     }
 
     return result.sort(([dateA], [dateB]) => dateA - dateB);
   });
+
+  const [activeIndex, setActiveIndex] = useState();
 
   const handleRef = useCallback((ref) => {
     if (!ref) return;
@@ -31,13 +45,28 @@ const DaysBar = ({ value, onSelect }) => {
     [onSelect]
   );
 
+  useEffect(() => {
+    const primeTimeItem = times.find((t) => t[2] === "primetime");
+    if (
+      primeTimeItem &&
+      value >= primeTimeItem[0].getTime() &&
+      value < addHours(primeTimeItem[0], 4)
+    ) {
+      setActiveIndex(times.indexOf(primeTimeItem));
+    } else {
+      const index = times.findIndex(
+        (t) => value > startOfDay(t[0]) && value < endOfDay(t[0])
+      );
+      setActiveIndex(index);
+    }
+  }, [times, value]);
+
   return (
     <div ref={handleRef} className="days-bar">
       {times.map((item, index) => {
         const date = item[0];
-        const label = item.length === 2 ? item[1] : format(date, "dd/MM");
-        const isActive =
-          value > date.getTime() && value < times[index + 1][0].getTime();
+        const label = item[1] ? item[1] : format(date, "dd/MM");
+        const isActive = index === activeIndex;
         return (
           <button
             key={item}
